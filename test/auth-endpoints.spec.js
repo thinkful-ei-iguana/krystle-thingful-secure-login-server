01166827980079a67f3c90aa0861f52e8cf4233c
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2019-12-20 12:12:34 -0500

    update

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -30,6 +30,38 @@ describe.only('Auth Endpoints', function() {
       )
     )
 
-    it('has a test')
+    const requiredFields = ['user_name', 'password']
+
+ requiredFields.forEach(field => {
+   const loginAttemptBody = {
+     user_name: testUser.user_name,
+     password: testUser.password,
+   }
+
+   it(`responds with 400 required error when '${field}' is missing`, () => {
+     delete loginAttemptBody[field]
+
+     return supertest(app)
+       .post('/api/auth/login')
+       .send(loginAttemptBody)
+       .expect(400, {
+         error: `Missing '${field}' in request body`,
+       })
+     })
+  })
+   it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+      const userInvalidUser = { user_name: 'user-not', password: 'existy' }
+      return supertest(app)
+      .post('/api/auth/login')
+      .send(userInvalidUser)
+      .expect(400, { error: `Incorrect user_name or password` })
+    })
+    it(`responds 400 'invalid user_name or password' when bad password`, () => {
+        const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
+        return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidPass)
+         .expect(400, { error: `Incorrect user_name or password` })
+      +   })
   })
 })
